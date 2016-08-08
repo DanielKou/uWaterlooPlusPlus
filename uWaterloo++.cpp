83b9182ab372fd5a13f0d3f7920fb9f27bb877b0
Daniel Kou <danielkou131@gmail.com>
2016-08-07 21:13:49 -0400

    courses endpoint

--- uWaterloo++.cpp
+++ uWaterloo++.cpp
@@ -1,6 +1,6 @@
 #include "uWaterloo++.h"
 
-
+// callback function that writes to string and returns size
 static size_t writeCallback(void *Contents, size_t size, size_t nmemb, void *userp) {
 	((std::string *)userp)->append((char*)Contents);
 	return size * nmemb;
@@ -15,11 +15,17 @@ void MapToVec(const  M & m, V & v) {
 	}
 }
 
-std::string nullCheck(json j) {
-	if (j.is_null())
-		return "";
-	else
-		return j.get<std::string>();
+
+// backup string parser for the json
+std::string nullCheck(json& j) {
+	return (j.is_null()) ? "" : j.get<std::string>();
+}
+
+
+// lowercase conversion
+std::string toLower(std::string str) {
+	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+	return str;
 }
 
 
@@ -49,6 +55,18 @@ EventsParser uWaterloo::parseEvents() {
 	result = result.substr(0, found + 1);
 	EventsParser ep(result, apiKey);
 	return ep;
+}
+
+CoursesParser uWaterloo::parseCourses() {
+	std::string url = "https://api.uwaterloo.ca/v2/courses.json?key=" + apiKey;
+	curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+	res = curl_easy_perform(curl);
+
+	// temp fix: clean up response, sometimes it screws up and appends garbage value (maybe look into why it happens)
+	size_t found = result.find_last_of("}");
+	result = result.substr(0, found + 1);
+	CoursesParser cp(result, apiKey);
+	return cp;
 }
 
 bool uWaterloo::cleanUp() {
@@ -87,8 +105,9 @@ std::ostream& operator << (std::ostream &out, Parser &p) {
 //-------------------------------------------------------------------------------------
 EventsParser::EventsParser(std::string eventsJson, std::string apiKey) : Parser(eventsJson, apiKey) {
 	parser = parser["data"];
-	length = parser.size();
+	size = parser.size();
 
+	// initialize events array
 	for (json::iterator it = parser.begin(); it != parser.end(); ++it) {
 		int id = (*it)["id"];
 		std::string site = (*it)["site"];
@@ -99,15 +118,12 @@ EventsParser::EventsParser(std::string eventsJson, std::string apiKey) : Parser(
 }
 
 
-int EventsParser::getLength() {
-	return length;
+int EventsParser::getSize() {
+	return size;
 }
 
 
-
-
-
-
+// filter by site
 std::vector<Event*> EventsParser::findEventsBySite(std::string site) {
 	std::vector<Event*> v;
 	
@@ -118,6 +134,7 @@ std::vector<Event*> EventsParser::findEventsBySite(std::string site) {
 }
 
 
+// filter by id
 std::vector<Event*> EventsParser::findEventsById(int id) {
 	std::vector<Event*> v;
 
@@ -135,8 +152,9 @@ std::vector<Event*> EventsParser::findEventsById(int id) {
 
 Event* EventsParser::findEvent(int id, std::string site) {
 	std::map<std::string, std::map<int, Event*> >::iterator it = events.find(site);
-	if (it != events.end() && it->second.find(id) != it->second.end()) {
-		return (it->second.find(id))->second;
+	if (it != events.end()) {
+		std::map<int, Event*>::iterator it2 = it->second.find(id);
+		return (it2 != it->second.end()) ? it2->second : NULL;
 	}
 	else {
 		return NULL;
@@ -146,7 +164,6 @@ Event* EventsParser::findEvent(int id, std::string site) {
 
 std::vector<Event> EventsParser::getEventsList() {
 	std::vector<Event> v;
-
 	for (std::map<std::string, std::map<int, Event*> >::iterator it = events.begin(); it != events.end(); it++) {
 		std::map<int, Event*> temp = it->second;
 		for (std::map<int, Event*>::iterator it2 = temp.begin(); it2 != temp.end(); it2++) {
@@ -168,6 +185,8 @@ std::vector<std::pair<std::string, std::string> > EventsParser::getHolidays() {
 		curl_easy_setopt(tempCurl, CURLOPT_WRITEDATA, &tempResult);
 		curl_easy_setopt(tempCurl, CURLOPT_URL, ("https://api.uwaterloo.ca/v2/events/holidays.json?key=" + apiKey).c_str());
 		curl_easy_perform(tempCurl);
+		
+		// initialize json object
 		json tempParser;
 		std::stringstream ss;
 		ss << tempResult;
@@ -177,6 +196,7 @@ std::vector<std::pair<std::string, std::string> > EventsParser::getHolidays() {
 		for (json::iterator it = tempParser.begin(); it != tempParser.end(); ++it) {
 			v.push_back(std::make_pair((*it)["name"].get<std::string>(), (*it)["date"].get<std::string>()));
 		}
+		// always cleanup
 		curl_easy_cleanup(tempCurl);
 	}
 	return v;
@@ -187,15 +207,15 @@ std::vector<std::pair<std::string, std::string> > EventsParser::getHolidays() {
 //-------------------------------------------------------------------------------------
 Event::Event(std::string eventJson, std::string apiKey, bool specific /* = false */) : Parser(eventJson, apiKey){
 	id = parser["id"];
-	title = parser["title"].get<std::string>(); //explicit string conversions
-	site = nullCheck((specific) ? parser["site_id"] : parser["site"]);
+	title = nullCheck(parser["title"]); 
+	site = nullCheck((specific) ? parser["site_id"] : parser["site"]); // key differs in event and specific event... thanks waterloo api
 	siteName = nullCheck(parser["site_name"]);
-	link = parser["link"].get<std::string>();
-	updatedTime = parser["updated"].get<std::string>();
+	link = nullCheck(parser["link"]);
+	updatedTime = nullCheck(parser["updated"]);
 }
 
 
-SpecificEvent Event::getSpecifics() {
+SpecificEvent Event::getMoreDetails() {
 	std::string tempResult;
 	CURL* tempCurl = curl_easy_init();
 	if (tempCurl) {
@@ -205,8 +225,10 @@ SpecificEvent Event::getSpecifics() {
 												+ site + std::string("/") + std::to_string(id) 
 												+ std::string(".json?key=") + apiKey).c_str());
 		curl_easy_perform(tempCurl);
+		// always cleanup
 		curl_easy_cleanup(tempCurl);
 
+		// initialize json object
 		json tempJson;
 		std::stringstream ss;
 		ss << tempResult;
@@ -230,29 +252,32 @@ std::string Event::getUpdatedTime() { return updatedTime; }
 //------------------------------ SPECIFIC EVENT ---------------------------------------
 //-------------------------------------------------------------------------------------
 SpecificEvent::SpecificEvent(std::string eventJson, std::string apiKey) : Event(eventJson, apiKey, true) {
-	description = parser["description"].get<std::string>();
-	descriptionRaw = parser["description_raw"].get<std::string>();
-	cost = (parser["cost"].is_null()) ? 0 : parser["cost"];
+	description = nullCheck(parser["description"]);
+	descriptionRaw = nullCheck(parser["description_raw"]);
+	cost = (parser["cost"].is_number()) ? parser["cost"] : 0;
+
+	// get list of times
 	json temp = parser["times"];
 	for (json::iterator it = temp.begin(); it != temp.end(); ++it) {
-		times.push_back(std::make_pair((*it)["start"].get<std::string>(), (*it)["end"].get<std::string>()));
+		times.push_back(std::make_pair(nullCheck((*it)["start"]), nullCheck((*it)["end"])));
 	}
 
+	// get list of types, save as comma seperated string
 	temp = parser["type"];
 	int size = temp.size();
 	for (int i = 0; i < size; i++) {
-		type += temp[i].get<std::string>();
 		if (i != 0) {
-			type += ", ";
+			type += ",";
 		}
+		type += nullCheck(temp[i]);
 	}
 
 	temp = parser["location"];
-	location.name = temp["name"].get<std::string>();
-	location.street = temp["street"].get<std::string>();
-	location.city = temp["city"].get<std::string>();
-	location.province = temp["province"].get<std::string>();
-	location.postalCode = temp["postal_code"].get<std::string>();
+	location.name = nullCheck(temp["name"]);
+	location.street = nullCheck(temp["street"]);
+	location.city = nullCheck(temp["city"]);
+	location.province = nullCheck(temp["province"]);
+	location.postalCode = nullCheck(temp["postal_code"]);
 }
 
 
@@ -263,3 +288,167 @@ double SpecificEvent::getCost() { return cost; }
 std::vector<std::pair<std::string, std::string> > SpecificEvent::getTimes() { return times; }
 std::string SpecificEvent::getType() { return type; }
 Location SpecificEvent::getLocation() { return location; }
+
+
+
+//------------------------------ COURSES PARSER ---------------------------------------
+//-------------------------------------------------------------------------------------
+CoursesParser::CoursesParser(std::string coursesJson, std::string apiKey) : Parser(coursesJson, apiKey) {
+	parser = parser["data"];
+	size = parser.size();
+
+	for (json::iterator it = parser.begin(); it != parser.end(); ++it) {
+		std::string id = (*it)["course_id"];
+		std::string subject = (*it)["subject"];
+		std::string catalog = (*it)["catalog_number"];
+
+		Course *c = new Course((*it).dump(), apiKey);
+
+		// filter courses
+		courses_subject[subject].insert(std::make_pair(catalog, c));
+		courses_id.insert(std::make_pair(id, c));
+	}
+}
+
+
+int CoursesParser::getSize() {
+	return size;
+}
+
+
+Course* CoursesParser::findCourse(std::string id) {
+	std::map<std::string, Course*>::iterator it = courses_id.find(id);
+	return (it != courses_id.end()) ? it->second : NULL;
+}
+
+
+Course*  CoursesParser::findCourse(std::string subject, std::string catalog) {
+	std::map<std::string, std::map<std::string, Course*> >::iterator it = courses_subject.find(subject);
+	if (it != courses_subject.end()) {
+		std::map<std::string, Course*>::iterator it2 = it->second.find(catalog);
+		return (it2 != it->second.end()) ? it2->second : NULL;
+	}
+	else {
+		return NULL;
+	}
+}
+
+
+std::vector<Course*> CoursesParser::findCoursesBySubject(std::string subject) {
+	std::vector<Course*> c;
+
+	if (courses_subject.find(subject) != courses_subject.end()) {
+		MapToVec(courses_subject[subject], c);
+	}
+	return c;
+}
+
+
+std::vector<Course> CoursesParser::getCourseList() {
+	std::vector<Course> c;
+	for (std::map<std::string, Course*>::iterator it = courses_id.begin(); it != courses_id.end(); it++) {
+		Course e = *(it->second);
+		c.push_back(e);
+	}
+	return c;
+}
+
+
+
+//------------------------------ COURSE -----------------------------------------------
+//-------------------------------------------------------------------------------------
+Course::Course(std::string courseJson, std::string apiKey) : Parser(courseJson, apiKey) {
+	id = nullCheck(parser["course_id"]);
+	subject = nullCheck(parser["subject"]);
+	catalog = nullCheck(parser["catalog_number"]);
+	title = nullCheck(parser["title"]);
+}
+
+
+// getters
+std::string Course::getId() { return id; }
+std::string Course::getSubjectAndCatalog() { return subject + " " + catalog; }
+std::string Course::getTitle() { return title; }
+
+
+SpecificCourse Course::getMoreDetails() {
+	std::string tempResult;
+	CURL* tempCurl = curl_easy_init();
+	if (tempCurl) {
+		curl_easy_setopt(tempCurl, CURLOPT_WRITEFUNCTION, writeCallback);
+		curl_easy_setopt(tempCurl, CURLOPT_WRITEDATA, &tempResult);
+		curl_easy_setopt(tempCurl, CURLOPT_URL, ("https://api.uwaterloo.ca/v2/courses/" + id + std::string(".json?key=") + apiKey).c_str());
+		curl_easy_perform(tempCurl);
+		// always cleanup
+		curl_easy_cleanup(tempCurl);
+
+		// initialize json object
+		json tempJson;
+		std::stringstream ss;
+		ss << tempResult;
+		ss >> tempJson;
+		SpecificCourse sc(tempJson["data"].dump(), apiKey);
+		return sc;
+	}
+}
+
+
+
+
+
+
+
+//------------------------------ SPECIFIC COURSE --------------------------------------
+//-------------------------------------------------------------------------------------
+SpecificCourse::SpecificCourse(std::string courseJson, std::string apiKey) : Course(courseJson, apiKey) {
+	units = (parser["units"].is_number()) ? parser["units"] : 0;
+	description = nullCheck(parser["description"]);
+
+	int size = parser["terms_offered"].size();
+	for (int i = 0; i < size; i++) {
+		if (i != 0) {
+			termsOffered += ",";
+		}
+		termsOffered += nullCheck(parser["terms_offered"][i]);
+	}
+
+	size = parser["instructions"].size();
+	for (int i = 0; i < size; i++) {
+		if (i != 0) {
+			termsOffered += ",";
+		}
+		instructions += nullCheck(parser["instructions"][i]);
+	}
+
+	url = nullCheck(parser["url"]);
+	academicLevel = nullCheck(parser["academic_level"]);
+}
+
+
+// getters
+std::string SpecificCourse::getDescription() { return description; }
+std::string SpecificCourse::getTermsOffered() { return termsOffered; }
+std::string SpecificCourse::getInstructionTypes() { return instructions; }
+std::string SpecificCourse::getUrl() { return url; }
+std::string SpecificCourse::getAcademicLevel() { return academicLevel; }
+
+
+bool SpecificCourse::isOffered(std::string term) {
+	std::string findTerm;
+	term = toLower(term);
+	if (term == "fall" || term == "f") {
+		findTerm = "F";
+	}
+	else if (term == "winter" || term == "w") {
+		findTerm = "W";
+	}
+	else if (term == "spring" || term == "s") {
+		findTerm = "S";
+	}
+
+	if (findTerm != "") {
+		return (termsOffered.find(findTerm) != std::string::npos);
+	}
+
+	return false;
+}
--- uWaterloo++.h
+++ uWaterloo++.h
@@ -25,47 +25,50 @@ struct Location {
 class EventsParser;
 class Event;
 class SpecificEvent;
+class CoursesParser;
+class Course;
+class SpecificCourse;
 
 class uWaterloo {
 	CURL *curl;
 	CURLcode res;
 	const std::string apiKey;
 	std::string result;
-
 public:
 	uWaterloo(std::string key);
 	bool init();
 	bool cleanUp();
-	EventsParser parseEvents();
+	EventsParser parseEvents(); // returns parser for Events endpoint
+	CoursesParser parseCourses();
 };
 
 
+// base parser class, defines base functionality
 class Parser {
 	const std::string raw;
 protected:
 	const std::string apiKey;
 	json parser;
-
 public:
 	Parser(std::string json, std::string apiKey);
-	std::string getRaw();
-	friend std::ostream& operator<<(std::ostream& out, Parser &p);
+	std::string getRaw(); // gets raw json
+	friend std::ostream& operator<<(std::ostream& out, Parser &p); // overloaded output stream operator
 };
+
 
 // NOTE: are copy constructor + assignment operator needed? shallow copy is ok, since all info is read only,
 // nothing will change
 class EventsParser : public Parser {
-	int length;
+	int size;
 	std::map<std::string, std::map<int, Event*> > events; // events filtered by site, then by id
-
 public:
 	EventsParser(std::string eventsJson, std::string apiKey);
-	int getLength();
-	std::vector<Event*> findEventsBySite(std::string site);
-	std::vector<Event*> findEventsById(int id);
-	Event* findEvent(int id, std::string site);
-	std::vector<Event> getEventsList();
-	std::vector<std::pair<std::string, std::string> > getHolidays();
+	int getSize(); // returns number of events, which is size of getEventsList()
+	std::vector<Event*> findEventsBySite(std::string site); // returns list of events at that site
+	std::vector<Event*> findEventsById(int id); // returns list of events with that id
+	Event* findEvent(int id, std::string site); // returns a pointer to the event or null
+	std::vector<Event> getEventsList(); // returns all events
+	std::vector<std::pair<std::string, std::string> > getHolidays(); // returns list of holidays in format: <name, date>
 };
 
 
@@ -76,17 +79,16 @@ class Event : public Parser {
 	std::string siteName;
 	std::string link;
 	std::string updatedTime;
-
 public:
 	Event(std::string eventJson, std::string apiKey, bool specific = false);
-	SpecificEvent getSpecifics();
+	SpecificEvent getMoreDetails(); // get more info for this event
+	// getters
 	int getId();
 	std::string getTitle();
 	std::string getSite();
 	std::string getSiteName();
 	std::string getLink();
 	std::string getUpdatedTime();
-	
 };
 
 
@@ -99,6 +101,7 @@ class SpecificEvent : public Event {
 	struct Location location;
 public:
 	SpecificEvent(std::string eventJson, std::string apiKey);
+	// getters
 	std::string getDescription();
 	std::string getDescriptionRaw();
 	double getCost();
@@ -108,6 +111,54 @@ public:
 };
 
 
+class CoursesParser : public Parser {
+	int size;
+	std::map<std::string, std::map<std::string, Course*> > courses_subject; // filtered by subject, then by catalog
+	std::map<std::string, Course*> courses_id; // filtered by id
+public:
+	CoursesParser(std::string coursesJson, std::string apiKey);
+	int getSize();
+	Course* findCourse(std::string id); // returns pointer to course or null (search by id)
+	Course* findCourse(std::string subject, std::string catalog); // returns pointer to course or null (search by subject and catalog)
+	std::vector<Course*> findCoursesBySubject(std::string subject); // returns list of courses of that subject
+	std::vector<Course> getCourseList(); // returns list of courses
+
+};
+
+
+class Course : public Parser {
+	  std::string id;
+	  std::string subject;
+	  std::string catalog;
+	  std::string title;
+public:
+	Course(std::string courseJson, std::string apiKey);
+	SpecificCourse getMoreDetails(); // get more info for this course
+	// getters
+	std::string getId();
+	std::string getSubjectAndCatalog(); // eg. CS 135
+	std::string getTitle();
+};
+
+
+
+class SpecificCourse : public Course {
+	double units;
+	std::string description;
+	std::string termsOffered; // F,W,S comma delimited
+	std::string instructions; // LEC,LAB,TUT,TST comma delimited
+	std::string url;
+	std::string academicLevel;
+public:
+	SpecificCourse(std::string courseJson, std::string apiKey);
+	bool isOffered(std::string term); // checks whether course is offered that term (search by whole word or letter: Winter/W)
+	// getters
+	std::string getDescription();
+	std::string getTermsOffered();
+	std::string getInstructionTypes();
+	std::string getUrl();
+	std::string getAcademicLevel();
+};
 
 
 
